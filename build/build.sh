c1676a3bebe13b84949f5a3406b8e31bae808f92
dongbin <dongbin0625@163.com>
2024-09-10 09:20:39 +0800

    修改.gitignore

--- build/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# cmake .. -DCMAKE_TOOLCHAIN_FILE=../../platforms/armv5l/arm-linux-gnueabi.cmake -DCMAKE_BUILD_TYPE=Debug -DDISABLE_UT=OFF
-cmake .. -DCMAKE_TOOLCHAIN_FILE=platforms/armv5l/arm-linux-gnueabi.cmake -DCMAKE_BUILD_TYPE=Debug -DDISABLE_UT=OFF
